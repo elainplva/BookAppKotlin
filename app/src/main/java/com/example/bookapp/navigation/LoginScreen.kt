b55053ddb591df1acd3678faa5926486070a92f1
elainplva <s00250500@atu.ie>
2025-11-18 13:30:54 +0000

    setting up the app, adding the navigation for all the screens based on the wireframes

---
+++ app/src/main/java/com/example/bookapp/navigation/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.example.bookapp.navigation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.bookapp.navigation.Navigation
+import com.example.bookapp.ui.theme.BookAppTheme
+
